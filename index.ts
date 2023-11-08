09071713a485fb51a36b3aa086934e18934f508b
boss11887 <boss-boss2@hotmail.com>
2023-11-08 10:39:45 +0700

    migrate to bun

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import express from "express";
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
+import * as db from "./query";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.post("/hook", (req: Request, res: Response) => {
+  // All message events
+  const events = req.body.events;
+  console.log(events);
+  console.log("===============");
+
+  // UserID of Line API
+  const UserId = req.body.events[0].source.userId;
+  console.log("UserID = " + UserId);
+
+  // User message of Line API
+  const message = req.body.events[0].message.text;
+  console.log("Message = " + message);
+
+  console.log("-------------");
+  console.log("hel12312lo");
+  res.status(200).end();
+});
+
+app.get("/users", db.getUsers);
---
+++ query.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { Pool, QueryResult } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT),
+  database: process.env.DATABASE,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  ssl: true,
+});
+
+const getUsers = (req: Request, res: Response) => {
+  pool.query("SELECT * FROM pgtest", (error: Error, result: QueryResult) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(result.rows);
+  });
+};
+
+export { getUsers };
